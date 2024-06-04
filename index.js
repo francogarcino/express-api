5509d566bf0bf08018a6b05e17f2510f5b5a581a
Franco <francogarcino@gmail.com>
2024-06-04 18:12:05 -0300

    rollback

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ formatDate = (timestamp) => {
     const dateObj = new Date(timestamp);
     const day = (dateObj.getDate()).toString().padStart(2, '0');
     const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+    const year = dateObj.getFullYear();
     const hours = dateObj.getHours().toString().padStart(2, '0');
     const minutes = dateObj.getMinutes().toString().padStart(2, '0');
-    return "["+day+"/"+month+"/3935"+" at "+hours+":"+minutes+"]"
+    return "["+day+"/"+month+"/"+year+" at "+hours+":"+minutes+"]"
 }
 
 require('dotenv').config();
